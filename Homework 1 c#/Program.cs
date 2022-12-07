10c0d572f9408feab16ea6d22cd373fa6a5b9e6f
Agshinhummatov <agshinshh@code.edu.az>
2022-12-07 17:50:35 +0400

    Homework C#

---
+++ Homework 1 c#/Program.cs
@@ -0,0 +1,104 @@
+﻿//Homework 1
+
+
+//int n = 42;
+//if (n % 3 == 0)
+
+//{
+
+
+//}
+//if (n % 7 == 0)
+//{
+//    Console.WriteLine("Bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Bolunmur");
+//}
+
+
+
+
+//Homework 2
+
+
+
+//int n = 8;
+//int m = 16;
+//int result = 0;
+//if (n % 2 == 0)
+//{
+
+//}
+//if (m % 2 == 0)
+//{
+//    result = n + m;
+//    Console.WriteLine(result);
+//}
+//else
+//{
+//    Console.WriteLine("duz deyil");
+//}
+
+
+//Homework 3
+
+
+
+//int n = 3;
+//int m = 15;
+//int count = 0;
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 1)
+
+//    {
+//        count = count + i;
+
+//    }
+//}
+
+//Console.WriteLine(count);
+
+
+
+
+//Homework 4
+
+
+
+
+//int[] numbers = { 1, 2, 3, 4, 5, 6, 10, 11,12,13,14,15 };
+//int sum = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] % 2 == 1)
+//    {
+//        sum = sum + numbers[i];
+//    }
+
+//}
+//Console.WriteLine(sum);
+
+
+
+
+//Homework 5
+
+
+
+
+//int[] numbers = { 1, 2, 3, 4, 5, 6, 10, 11, 12, 13, 14, 15 };
+//int count = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] % 2 == 0)
+//    {
+//        count++;
+//    }
+
+//}
+//Console.WriteLine(count);
+
+
